b28f6340c08054426c60cdb851cb56d44076a0ea
noobdy <htommyf12@gmail.com>
2023-02-28 22:51:33 +0800

    clean up trash

--- maze.py
+++ maze.py
@@ -1,30 +1,32 @@
-import numpy as np
+import sys
 from itertools import chain
 import random
+
+import numpy as np
 import matplotlib.pyplot as plt
-import sys
+
 
 def dfs(rooms, roads):
     start = np.array([random.randrange(x) for x in rooms.shape])
-    while rooms[tuple(start)]==1:
+    while rooms[tuple(start)] == 1:
         start = np.array([random.randrange(x) for x in rooms.shape])
 
     stk = [start]
     while stk:
         current = stk.pop()
-        directs = [[0,1,0],
-                   [0,-1,0],
-                   [0,0,1],
-                   [0,0,-1]]
+        directs = [[0, 1, 0],
+                   [0, -1, 0],
+                   [0, 0, 1],
+                   [0, 0, -1]]
         if rooms[tuple(current)] == 0:
-            directs.extend([[1,1,0],
-                            [1,-1,0],
-                            [1,0,1],
-                            [1,0,-1],
-                            [-1,1,0],
-                            [-1,-1,0],
-                            [-1,0,1],
-                            [-1,0,-1]])
+            directs.extend([[1, 1, 0],
+                            [1, -1, 0],
+                            [1, 0, 1],
+                            [1, 0, -1],
+                            [-1, 1, 0],
+                            [-1, -1, 0],
+                            [-1, 0, 1],
+                            [-1, 0, -1]])
         while True:
             if not directs:
                 break
@@ -32,71 +34,77 @@ def dfs(rooms, roads):
             directs.remove(list(p))
             n = current + p
             try:
-                if rooms[tuple(n)] in (2,3): continue
-                if p[0] != 0:
-                    n = current + p*[1,2,2]
-                    rooms[tuple(n)]
-                    if rooms[tuple(current + p*[0,1,1])] in (2,3,4):
-                        continue
-                    if rooms[tuple(current + p*[1,1,1])] in (2,3,4):
-                        continue
-                if list(n) in map(list,chain.from_iterable(roads)):
+                if rooms[tuple(n)] in (2, 3):
                     continue
-                if (n<0).any(): continue
+                if p[0] != 0:
+                    n = current + p * [1, 2, 2]
+
+                    if rooms[tuple(current + p * [0, 1, 1])] in (2, 3, 4):
+                        continue
+                    if rooms[tuple(current + p * [1, 1, 1])] in (2, 3, 4):
+                        continue
+                if list(n) in map(list, chain.from_iterable(roads)):
+                    continue
+                if (n < 0).any():
+                    continue
             except IndexError:
                 continue
-            #complete
+            # complete
             if p[0] != 0:
-                if rooms[tuple(current + p*[0,1,1])] == 0:
+                if rooms[tuple(current + p * [0, 1, 1])] == 0:
                     print(current, n)
                     input()
-                rooms[tuple(current + p*[0,1,1])] = 2
-                rooms[tuple(current + p*[1,1,1])] = 3
+                rooms[tuple(current + p * [0, 1, 1])] = 2
+                rooms[tuple(current + p * [1, 1, 1])] = 3
             elif rooms[tuple(n)] == 1:
                 rooms[tuple(n)] = 4
 
-            roads.append((current,n))
+            roads.append((current, n))
             stk.append(current)
             stk.append(n)
             break
+
 
 def display(roads, h):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     colors = [np.random.rand(3,) for _ in range(h)]
-    for r in roads:
-        b = np.array([[0,0,1],
-                      [1,0,0],
-                      [0,1,0]])
-        r = np.matmul(r,b)
-        ax.plot3D(*np.array(r).T, c=colors[r.T[2].min()])
+    for road in roads:
+        b = np.array([[0, 0, 1],
+                      [1, 0, 0],
+                      [0, 1, 0]])
+        road = np.matmul(road, b)
+        ax.plot3D(*np.array(road).T, c=colors[road.T[2].min()])
     plt.show()
 
+
 if __name__ == '__main__':
-    h,w,d = 5,5,5 # default parameters
+    h, w, d = 5, 5, 5  # default parameters
 
     arg = tuple(map(int, sys.argv[1:]))
     if len(arg) == 3:
-        h,w,d = arg
+        h, w, d = arg
     elif len(arg) != 0:
         sys.exit("Wrong arguments")
 
-    isEven = lambda x : x&1 == 0
+    def isEven(x):
+        return x & 1 == 0
+
     if isEven(w) or isEven(d):
         sys.exit("Arguments width or depth cannot be even number")
 
-    print(h,w,d)
+    print(h, w, d)
     rooms = np.array([
-            np.array([x&1 for x in range(w*d)]).reshape(w,d)
-            for x in range(h)])
+        np.array([x & 1 for x in range(w * d)]).reshape(w, d)
+        for x in range(h)])
     stairs = []
     roads = []
     dfs(rooms, roads)
-    #print(rooms)
+    # print(rooms)
     print(len(roads))
     roads = sorted(roads, key=lambda x: x[0][0])
     for r in roads:
         print(r)
-    #print(sorted(roads, key=lambda x,y: x[0]))
+    # print(sorted(roads, key=lambda x,y: x[0]))
     display(roads, h)
     sys.exit()
